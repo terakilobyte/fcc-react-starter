83949cd7f4b08bb1d50e53c4a502d2ca6d5dacfc
terakilobyte <terakilobyte@gmail.com>
2016-07-29 02:20:23 -0700

    Wire up all the things

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { render } from 'react-dom';
+/*eslint-disable no-unused-vars */
+import React from 'react'; //needed for JSX
+import App from './components/App';
+/*eslint-enable */
+
+render(
+  <App />, document.getElementById('app')
+);
---
+++ src/webpack-public-path.js
@@ -0,0 +1,2 @@
+/* eslint-disable */
+__webpack_public_path__ = window.location.protocol + '//' + window.location.host + '/';
